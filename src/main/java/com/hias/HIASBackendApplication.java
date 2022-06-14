0f62b095de812633f9c6773a2c798840f22a4cb6
mob141043 <mob5832@gmail.com>
2022-06-14 22:10:10 +0700

    Initialize project

---
+++ src/main/java/com/hias/HIASBackendApplication.java
@@ -0,0 +1,13 @@
+package com.hias;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HIASBackendApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(HIASBackendApplication.class, args);
+    }
+
+}
