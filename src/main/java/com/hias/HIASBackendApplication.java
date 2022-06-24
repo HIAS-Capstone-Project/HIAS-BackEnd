6ed061fa670b4c861cc32679e410b0734abc15a1
BuiVietCuong <buivietcuong0410@gmail.com>
2022-06-24 16:22:49 +0700

    [Feature][HIAS-3] Config and test Email Sender, have not completed yet. Do not merge

--- src/main/java/com/hias/HIASBackendApplication.java
+++ src/main/java/com/hias/HIASBackendApplication.java
@@ -1,13 +1,30 @@
 package com.hias;
 
+import com.hias.service.EmailSenderService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.context.event.ApplicationReadyEvent;
+import org.springframework.context.event.EventListener;
+
+import javax.mail.MessagingException;
 
 @SpringBootApplication
 public class HIASBackendApplication {
+
+    @Autowired
+    private EmailSenderService emailSenderService;
 
     public static void main(String[] args) {
         SpringApplication.run(HIASBackendApplication.class, args);
     }
 
+    @EventListener(ApplicationReadyEvent.class)
+    public void triggerEmail() throws MessagingException {
+        emailSenderService.sendEmailWithAttachment(
+                "vietcuongtx@gmail.com",
+                "This is body",
+                "Subject of email",
+                "\"C:\\Users\\ASUS\\Desktop\\For sending.txt\"");
+    }
 }
