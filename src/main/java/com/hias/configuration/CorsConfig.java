72e0e466f332cdc8d2d90d48111b7287be8bef1e
mob141043 <mob5832@gmail.com>
2022-08-03 16:39:21 +0700

    Fix Cors issue

---
+++ src/main/java/com/hias/configuration/CorsConfig.java
@@ -0,0 +1,2 @@
+package com.hias.configuration;public class CorsConfig {
+}
---
+++ src/main/java/com/hias/constant/HttpsConstant.java
@@ -0,0 +1,9 @@
+package com.hias.constant;
+
+public class HttpsConstant {
+    public static final int SUCCESS_CODE = 200;
+    public static final String GET = "GET";
+    public static final String POST = "POST";
+    public static final String PUT = "PUT";
+    public static final String DELETE = "DELETE";
+}
