d9ece2c0912c4fcf223a78de4fd4f3a030119a03
mob141043 <mob5832@gmail.com>
2022-08-03 16:49:39 +0700

    Fix build fail issue

--- src/main/java/com/hias/constant/HttpsConstant.java
+++ src/main/java/com/hias/constant/HttpsConstant.java
@@ -1,9 +1,13 @@
 package com.hias.constant;
 
 public class HttpsConstant {
-    public static final int SUCCESS_CODE = 200;
+
+    private HttpsConstant() {
+    }
+
     public static final String GET = "GET";
     public static final String POST = "POST";
     public static final String PUT = "PUT";
     public static final String DELETE = "DELETE";
+    public static final String PATCH = "PATCH";
 }
