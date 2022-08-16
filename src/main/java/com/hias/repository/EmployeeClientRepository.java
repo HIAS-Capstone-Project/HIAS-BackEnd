d70d52c01bd60e51aaf0c51d4368d6d884da30b8
Thanhh <thanh301120@gmail.com>
2022-08-16 16:25:14 +0700

    fix deleted for client

--- src/main/java/com/hias/repository/EmployeeClientRepository.java
+++ src/main/java/com/hias/repository/EmployeeClientRepository.java
@@ -10,4 +10,5 @@ import java.util.List;
 @Repository
 public interface EmployeeClientRepository extends JpaRepository<EmployeeClient, Long> {
     List<EmployeeClient> findByEmployeeNoAndIsDeletedIsFalse(Long employeeNo);
+    List<EmployeeClient> findByClientNoAndIsDeletedIsFalse(Long clientNo);
 }
