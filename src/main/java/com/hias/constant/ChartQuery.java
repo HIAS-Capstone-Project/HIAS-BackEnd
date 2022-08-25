50cc6e28074a181e47ee78909683907650c536b2
BuiVietCuong <buivietcuong0410@gmail.com>
2022-08-25 11:09:01 +0700

    [Feature][HIAS] - change to getmapping

--- src/main/java/com/hias/constant/ChartQuery.java
+++ src/main/java/com/hias/constant/ChartQuery.java
@@ -22,7 +22,7 @@ public class ChartQuery {
     public static final String MEMBER_LOCATION_CHART_QUERY = "SELECT province_name key, COUNT(member_name) value " +
             "FROM HIAS.DISTRICT d INNER JOIN HIAS.PROVINCE p ON d.province_no = p.province_no " +
             "INNER JOIN HIAS.MEMBER m ON d.district_no = m.district_no " +
-            "WHERE 1 = 1 %s" +
+            "WHERE 1 = 1 %s " +
             "GROUP BY province_name";
 
     public static final String MEMBER_ONBOARD_CHART_QUERY = "SELECT EXTRACT(YEAR FROM start_date) AS key, COUNT(member_no) value\n" +
