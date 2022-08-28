e35a724edcd7c23aefa3adc1d81df80ddba85a71
mob141043 <mob5832@gmail.com>
2022-08-28 07:57:44 +0700

    Add path variable for api find claim remark history by claim no

--- src/main/java/com/hias/controller/ClaimRemarkHistoryController.java
+++ src/main/java/com/hias/controller/ClaimRemarkHistoryController.java
@@ -7,6 +7,7 @@ import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -21,7 +22,7 @@ public class ClaimRemarkHistoryController {
     private ClaimRemarkHistoryService claimRemarkHistoryService;
 
     @GetMapping("find-by-claim-no/{claimNo}")
-    public ResponseEntity<List<ClaimRemarkHistoryResponseDTO>> findByClaimNo(Long claimNo) {
+    public ResponseEntity<List<ClaimRemarkHistoryResponseDTO>> findByClaimNo(@PathVariable Long claimNo) {
         return new ResponseEntity<>(claimRemarkHistoryService.findByClaimNo(claimNo), HttpStatus.OK);
     }
 }
